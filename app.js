5eaf13f7ce6aac581b280eede29e0f27d825320b
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-10-12 19:43:33 -0700

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+var options= {
+    invertx: false,
+    invertv: false,
+    limitx: 40, 
+    limitv: 40
+}
+
+var emoji = document.getElementById('emoji');
+var parallax = new parallax (emoji,options); 
+
