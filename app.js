cb1bb8dc5dac733614420e2e87bba59c63d2cab6
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-10-13 00:35:15 -0700

    test

--- app.js
+++ app.js
@@ -10,3 +10,8 @@ var emoji = document.getElementById('emoji');
 document.querySelector('button').addEventListener('keyup', (event)=>{
     event.targer.textContent = 'I was clicked';
 })
+document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyI' && (event.ctrlKey || event.metaKey)) {
+      alert('Undo!')
+    }
+  });
