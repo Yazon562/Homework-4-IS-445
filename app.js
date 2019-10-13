364b8c41ccad95e893a7873a31794ec1a3b4939f
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-10-13 13:14:57 -0700

    test

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const el = document.getElementById('emoji')
+const keyboardInfo=I=>{
+        console.log('Keyboard event:${I.type},key:${I.key},code:${I.code'); 
+        };
+        console.log(event.type);
+        console.log(event.keyCode);
+        console.log(String.fromCharCode(event.keyCode));
+
+        
+        document.addEventListener('keypress',keyboardInfo);
+        document.addEventListener('keydown',keyboardInfo);
+        document.addEventListener('keyup',keyboardInfo);
+
+
+    }
+}
