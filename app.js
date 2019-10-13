0d2417878fc7e619872f1bec7889a7eb5a15a195
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-10-13 16:38:49 -0700

    test

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const keyboardInfo=I=>{
         console.log(event.keyCode);
         console.log(String.fromCharCode(event.keyCode));
 
-        
-        document.addEventListener('keypress',keyboardInfo);
-        document.addEventListener('keydown',keyboardInfo);
         document.addEventListener('keyup',keyboardInfo);
 
 
