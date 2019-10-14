55116198c1c7f969f31a545957079c1ef412f408
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-10-13 23:01:32 -0700

    test

--- app.js
+++ app.js
@@ -1,61 +1,17 @@
-
-// use event listener to detect + (107) or - (109)
-// use loop so when its +, add 15 px and if its - then subtract 15
-// when 0 or 60 is reached, its either done or add pop emoji
-
-document.addEventListener("keydown",sizeFunc,false) // records the key pressed by the user
-
-
-
-function sizeFunc(key){                  // checks which key is pressed and run function
-    if (key.keyCode == "70"){                   // run function that corresponds with the key pressed
-            bigFunc();
-            console.log("pressed f")
-        }
-    else if (key.keyCode == "65"){
-            smallFunc();
-        }
-    else {
-            console.log("error")
-         }
-    }
-
-
-
-
-
-
-function bigFunc(){
-    var id = document.getElementById("Balloon")
-    var fontSize = window.getComputedStyle(id,null).getPropertyValue('font-size')
-    var currentSize = parseFloat(fontSize)
-
-       // document.getElementById("Balloon").innerHTML="hello";
-       // document.getElementById("Balloon").style.fontSize= '100px'
-        if (currentSize < 60){
-            document.getElementById("Balloon").style.fontSize = (currentSize + 15) + 'px'
-        }
-        else {
-            document.getElementById("Balloon").textContent="🎉"
-            document.removeEventListener("keydown",sizeFunc)
-        }
-        
-    }
-
-function smallFunc(){
-    var id = document.getElementById("Balloon")
-    var fontSize = window.getComputedStyle(id,null).getPropertyValue('font-size')
-    var currentSize = parseFloat(fontSize)
-
-    if (currentSize > 15){
-        document.getElementById("Balloon").style.fontSize = (currentSize - 15) + 'px'
-    }
-    else {
-        document.getElementById("Balloon").style.fontSize = "20px";
-        document.getElementById("Balloon").textContent="Done"
-        document.removeEventListener("keydown",sizeFunc)
-
-    }
-
-
+const keyboardInfo = I => {
+        console.log(event.keyCode);
+        const emoji = document.getElementById('emoji');
+        let fs = 30; // pull this each time from the emoji element
+        if (event.keyCode === 73) {
+    
+            emoji.style.fontSize = fs + 10 + 'px';
+        }
+        if (event.keyCode == "68") {
+    
+            emoji.style.fontSize = fs - 10 + 'px';
+        }
+      
+    const el = document.getElementById('body');
+    el.addEventListener('keyup', keyboardInfo);
+    
     }
