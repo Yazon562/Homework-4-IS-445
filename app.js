2aae42ad738641383264fa755927a7a1cc0e2e8d
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-10-13 19:41:35 -0700

    test

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const keyboardInfo = I => {
     console.log(event.keyCode);
     const emoji = document.getElementById('emoji');
     let fs = 30; // pull this each time from the emoji element
-    if (event.keyCode = 73) {
+    if (event.keyCode === 73) {
 
         emoji.style.fontSize = fs + 10 + 'px';
     }
-    if (event.keyCode = "68") {
+    if (event.keyCode == "68") {
 
-        emoji.style.fontSize == fs - 10 + 'px';
+        emoji.style.fontSize = fs - 10 + 'px';
     }
 
     
