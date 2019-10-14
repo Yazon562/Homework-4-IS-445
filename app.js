e7a14388b68aa174b0c4a32ba5c5449102a4c1f8
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-10-13 18:36:07 -0700

    test

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const keyboardInfo=I=>{
-        console.log('Keyboard event:${I.type},key:${I.key},code:${I.code'); 
-        };
-        console.log(event.type);
-        console.log(event.keyCode);
-        console.log(String.fromCharCode(event.keyCode));
+const keyboardInfo = I => {
+    console.log(event.keyCode);
+    const emoji = document.getElementById('emoji');
+    let fs = 30; // pull this each time from the emoji element
+    if (event.keyCode === 73) {
 
-        document.addEventListener('keyup',keyboardInfo);
-
-
+        emoji.style.fontSize = fs + 10 + 'px';
     }
+    if (event.keyCode == "68") {
+
+        emoji.style.fontSize = fs - 10 + 'px';
+    }
+
+    // console.log(event.type);
+    // console.log(event.keyCode);
+    // console.log(String.fromCharCode(event.keyCode));
 }
+
+
+
+const el = document.getElementById('body');
+el.addEventListener('keyup', keyboardInfo);
